ec60481ee987441c27cb3044b9adbdcf8cbc8588
Andrew Pang <andrewpang94@gmail.com>
2016-03-02 15:33:58 -0800

    Added history interface

--- app/src/main/java/com/sniffit/sniffit/REST/ServerRequest.java
+++ app/src/main/java/com/sniffit/sniffit/REST/ServerRequest.java
@@ -149,6 +149,8 @@ public class ServerRequest {
         @POST("api/user/{user_id}/find/")
         Call<ResponseBody> find(@Header("x-access-token") String token, @Path("user_id") String userId, @Field("name") String name, @Field("roomName") String roomName);
 
+        @GET("api/user/{user_id}/history/{ref_tagId}")
+        Call<ResponseBody> getHistory(@Header("x-access-token") String token, @Path("user_id") String userId, @Path("ref_tagId") String tagId);
 
     }
 
@@ -233,6 +235,11 @@ public class ServerRequest {
             case "reference":
                 Call<ResponseBody> getReferenceCall = apiService.getSnapdragon(user.getToken(), user.getUserId(), id);
                 getReferenceCall.enqueue(callback);
+                break;
+
+            case "history":
+                Call<ResponseBody> getHistoryCall = apiService.getHistory(user.getToken(), user.getUserId(), id);
+                getHistoryCall.enqueue(callback);
                 break;
         }
 
